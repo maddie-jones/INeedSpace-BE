95a98e3c8fc4424945213634edd03040b6703ae5
Maddie Jones <maddyg91@gmail.com>
2019-04-13 21:24:56 -0600

    Adds get favorites endpoint Adds test for above All tests passing

--- spec/api/v1/favorites_spec.rb
+++ spec/api/v1/favorites_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 describe 'Favorites API' do
+  it 'should return all favorites' do
+    favorite_1 = Favorite.create(title: "star_1", url: "star_img_1.com", hdurl: "star_img_hd_1", explanation: "good photo of star 1")
+    favorite_2 = Favorite.create(title: "star_2", url: "star_img_2.com", hdurl: "star_img_hd_2", explanation: "good photo of star 2")
+
+    get '/api/v1/favorites'
+
+    favorites_json = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(favorites_json["data"][0]["attributes"]["explanation"]).to eq("good photo of star 1")
+    expect(favorites_json["data"][0]["attributes"]["url"]).to eq("star_img_1.com")
+    expect(favorites_json["data"][0]["attributes"]["title"]).to eq("star_1")
+    expect(favorites_json["data"][1]["attributes"]["explanation"]).to eq("good photo of star 2")
+    expect(favorites_json["data"][1]["attributes"]["url"]).to eq("star_img_2.com")
+    expect(favorites_json["data"][1]["attributes"]["title"]).to eq("star_2")
+  end
+
   it 'should add a Favorite image' do
     params = {
       title: 'galaxy',
