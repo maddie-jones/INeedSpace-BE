da82b0faefd06ebb2b5e74d7b614b9d72dc257b3
Maddie Jones <maddyg91@gmail.com>
2019-04-15 09:37:22 -0600

    Seeding to deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,6 @@ favorite_1 = Favorite.create({
   explanation: "Rigil Kentaurus is the bright star near the top of this broad southern skyscape. Of course it's probably better known as Alpha Centauri, nearest star system to the Sun. Below it sprawls a dark nebula complex. The obscuring interstellar dust clouds include Sandqvist catalog clouds 169 and 172 in silhouette against the rich starfields along the southern Milky Way. Rigil Kent is a mere 4.37 light-years away, but the dusty dark nebulae lie at the edge of the starforming Circinus-West molecular cloud about 2,500 light-years distant. The wide-field of view spans over 12 degrees (24 full moons) across southern skies.",
   url: "https://apod.nasa.gov/apod/image/1904/sandqvist169_colombari1024.jpg",
   hdurl: "https://apod.nasa.gov/apod/image/1904/sandqvist169_colombari.jpg",
-  title: "Rigil Kentaurus and Sandqvist 169"
+  title: "Rigil Kentaurus and Sandqvist 169",
+  media_type: "image"
   })
