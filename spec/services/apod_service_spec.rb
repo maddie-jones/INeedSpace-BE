ea1456885254d0d4372e1e84edb743cb2f6c38fc
Maddie Jones <maddyg91@gmail.com>
2019-04-15 14:09:54 -0600

    Changes test for apod service, updates cassettes

--- spec/services/apod_service_spec.rb
+++ spec/services/apod_service_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe 'NASA APOD api' do
   it 'can connect', :vcr do
     day_image = ApodService.day_image
-    expect(day_image[:date]).to eq("2019-04-13")
+    expect(day_image[:service_version]).to eq("v1")
     end
 end
