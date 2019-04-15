44e2bf29e537ed8a72848fcab80b9186825d499f
Maddie Jones <maddyg91@gmail.com>
2019-04-15 16:39:21 -0600

    Service test passing

--- spec/services/nasa_image_service_spec.rb
+++ spec/services/nasa_image_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'NASA image api' do
   it 'can connect', :vcr do
-    search_images = NasaImageService.search_images("mars")
-    expect(search_images[:collection][:metadata][:items][0][:media_type]).to eq("image")
+    search_images = NasaImageService.search_images("mars")    
+    expect(search_images[:collection][:items][0][:data][0][:media_type]).to eq("image")
   end
 end
