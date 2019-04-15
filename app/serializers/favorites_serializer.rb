1505672afec95d08153a4b38a00d71d9cfd1765f
Maddie Jones <maddyg91@gmail.com>
2019-04-15 10:13:36 -0600

    Adds media_type to get favorites endpoint

--- app/serializers/favorites_serializer.rb
+++ app/serializers/favorites_serializer.rb
@@ -1,4 +1,4 @@
 class FavoritesSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :explanation, :url, :hdurl, :title
+  attributes :explanation, :url, :hdurl, :title, :media_type
 end
