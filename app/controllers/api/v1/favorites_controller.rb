464ff319fa5f09d137ce73cd5dbaf4ec9ef9a757
Maddie Jones <maddyg91@gmail.com>
2019-04-14 21:21:18 -0600

    Adds seed

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::FavoritesController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def index
     favorites = Favorite.all
     render status: :ok, json: FavoritesSerializer.new(favorites)
   end
+  
   def create
     if favorite_params
       favorite = Favorite.create(favorite_params)
